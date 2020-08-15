b37146066e92fee965fd778496c58ba9c38b4fd4
Daniel Gil <danielgil@iMac-de-Daniel.local>
2020-08-15 17:28:42 +0200

    Realizado la opcion borrado pasamos a la de modificar

--- Jugadores.py
+++ Jugadores.py
@@ -13,19 +13,87 @@ def archivo_segundo():
         write = csv.writer(csv_file)
         for key, value in jugador_Segunda.items():
             write.writerow([Key, value])
+
+def archivo_segundoB():
+    with open("jugador_tres.csv", "w") as csv_file:
+        write = csv.writer(csv_file)
+        for key, value in jugador_Segunda.items():
+            write.writerow([Key, value])
+
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+
+def borrar_jugador():
+    jb= str(input("¿ Quieres borrar un jugador: Si / No \n==>"))
+    if jb == "si" or jb == "Si":
+        nombre= str(input(" Introduce el nombre del jugador:"))
+        jugador.pop(nombre)
+        with open("jugador_uno.csv", "w") as csv_file:
+            write = csv.writer(csv_file)
+            for Key, value in jugador.items():
+                write.writerow([Key, value])
+    elif jb == "no" or jb == "No":
+        archivo_primero()
+        pass
+    else:
+        print(" Error 405")
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+
+def modificar_jugador():
+
+
 #------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------
 def gestionar_jugadores():
     j = str(input("¿De qué categoría quieres gestionar la plantilla?\n1. Primera División\n2. Segunda División"
                   "\n3. SegundaB División\n==>"))
+
     if j == "1":
+        for i in range(20):
+            nombre = str(input("\nNombre del Jugador: "))
+            edad = str(input("Edad: "))
+            posicion = str(input("Posicion: "))
+            nacionalidad = str(input("Nacionalidad: "))
+            jugador[nombre] = edad, posicion, nacionalidad
+            seguir= str(input("¿Quiere seguir añadiendo jugadores?: SI/No"))
+            if seguir == "si" or seguir == "Si":
+                archivo_primero()
+                pass
+            elif seguir == "no" or seguir == "No":
+                seguir_dos=str(input(" Qué opción desea ejecutar?: (Borrar)/(modificar)/(salir)\n ==>"))
+                if seguir_dos == "borrar" or seguir_dos == "Borrar":
+                    borrar_jugador()
+                    archivo_primero()
+                    seguir_tres = str(input("¿Quieres seguir añadiendo jugadores? Si / No"))
+                    if seguir_tres == "si" or seguir_tres == "Si":
+                        pass
+                    else:
+                        exit()
+            elif seguir_dos == "modificar" or seguir_dos == "Modificar":
+
+
+
+
+
+
+
+
+
+    """if j == "1":
         nombre = str(input("\nNombre del Jugador: "))
         edad = str(input("Edad: "))
         posicion = str(input("Posicion: "))
         nacionalidad = str(input("Nacionalidad: "))
         jugador[nombre]=edad, posicion, nacionalidad
         archivo_primero()
+        borrar_jugador()
+        archivo_primero()
+
+
 
 
     elif j == "2":
@@ -34,6 +102,7 @@ def gestionar_jugadores():
         posicion = str(input("Posicion: "))
         nacionalidad = str(input("Nacionalidad: "))
         jugador_Segunda[nombre] = edad, posicion, nacionalidad
+        archivo_segundo()
 
     elif j == "3":
         nombre = str(input("Nombre del Jugador: "))
@@ -41,3 +110,4 @@ def gestionar_jugadores():
         posicion = str(input("Posicion: "))
         nacionalidad = str(input("Nacionalidad: "))
         jugador_SegundaB[nombre] = edad, posicion, nacionalidad
+        archivo_segundoB()"""
